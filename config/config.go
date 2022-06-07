22aed697d4d6518eb97b77f201b2601a9259134c
Sangapan Panjaitan <sangapanpanjaitan@gmail.com>
2022-06-07 10:25:42 +0700

    Init Commit

--- config/config.go
+++ config/config.go
@@ -3,12 +3,14 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func ConnectDB() *sql.DB {
-
+	dbConnection := os.Getenv("DB_CONNECTION")
+	db, err := sql.Open("mysql", dbConnection)
 	if err != nil {
 		panic(err.Error())
 	} else {
