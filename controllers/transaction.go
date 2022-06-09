9f176d4dd2d8bc6a14ca49e6d52ea4210ccce49f
Artby <artbydhimathera@gmail.com>
2022-06-09 19:32:34 +0700

    Added Top-Up Feature

--- controllers/transaction.go
+++ controllers/transaction.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetBalancebyPhone(db *sql.DB, phone string) ([]_entities.User, error) {
-	fmt.Println("success")
+
 	query, err := db.Query(`SELECT balance FROM user WHERE phone = ?`, phone)
 
 	if err != nil {
@@ -28,4 +28,42 @@ func GetBalancebyPhone(db *sql.DB, phone string) ([]_entities.User, error) {
 	}
 
 	return user, nil
+}
+
+func TopUp(db *sql.DB, topup int, phone string) error {
+	_, err := db.Query(`INSERT INTO topup (TopUp_phone, TopUp_balance) VALUES (?, ?)`, phone, topup)
+
+	if err != nil {
+		fmt.Println("error1", err.Error())
+		return err
+	}
+	// test, err1 := db.Query(`SELECT Balance FROM user WHERE Phone = ? UNION ALL SELECT TopUp_Balance FROM topup WHERE TopUp_phone = ?`, phone, phone)
+	_, err1 := db.Query(`UPDATE user SET Balance = (SELECT Balance WHERE Phone = ?) + (SELECT TopUp_balance FROM topup ORDER BY Created_At DESC LIMIT 1) `, phone)
+	if err1 != nil {
+		fmt.Println("error2", err1.Error())
+		return err1
+	} else {
+		return nil
+	}
+}
+
+func GetNewBalance(db *sql.DB, phone string) ([]_entities.User, error) {
+	query, err := db.Query(`SELECT balance from user WHERE phone = ? AND phone is not null`, phone)
+
+	if err != nil {
+		fmt.Println("error1", err.Error())
+	}
+
+	var other []_entities.User
+
+	for query.Next() {
+		var data _entities.User
+		err := query.Scan(&data.Balance)
+
+		if err != nil {
+			fmt.Println("error2", err.Error())
+		}
+		other = append(other, data)
+	}
+	return other, nil
 }
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 	_config "project1/config"
-	_userController "project1/controllers"
+	_controller "project1/controllers"
 	_entities "project1/entities"
 )
 
@@ -62,17 +62,16 @@ func Login() {
 
 	phone = user.Phone
 	pass = user.Password
-
 	UserMenu(phone, pass)
 
 }
 
 //Menu User
 func UserMenu(phone string, pass string) {
-	resLogin, err = _userController.GetUserbyPhone(dbConn, phone, pass)
+	resLogin, err = _controller.GetUserbyPhone(dbConn, phone, pass)
 
 	if err != nil || resLogin == nil {
-		fmt.Println("==== Silakan cek kembali No. HP dan Password ====")
+		fmt.Println("===== Silakan cek kembali No. HP dan Password =====")
 	}
 
 	for _, value := range resLogin {
@@ -128,17 +127,17 @@ func Register() {
 	fmt.Println("Masukkan Address :")
 	fmt.Scanln(&newUser.Address)
 
-	err := _userController.CreateUser(dbConn, newUser)
+	err := _controller.CreateUser(dbConn, newUser)
 
 	if err != nil {
-		fmt.Println("==== Nomor HP sudah terdaftar ====")
+		fmt.Println("===== Nomor HP sudah terdaftar =====")
 	} else {
-		fmt.Println("==== Registrasi berhasil ====")
+		fmt.Println("===== Registrasi berhasil =====")
 	}
 }
 
 func UserProfile() {
-	resLogin, err = _userController.GetUserbyPhone(dbConn, phone, pass)
+	resLogin, err = _controller.GetUserbyPhone(dbConn, phone, pass)
 	for pilihan != "0" {
 		fmt.Println("===== Profile Menu =====")
 		fmt.Println("1. Read Profile")
@@ -177,9 +176,9 @@ func UserProfile() {
 }
 
 func ReadProfile() {
-	resLogin, err = _userController.GetUserbyPhone(dbConn, phone, pass)
+	resLogin, err = _controller.GetUserbyPhone(dbConn, phone, pass)
 	for _, value := range resLogin {
-		fmt.Printf("==== Profile of %s ====", value.Name)
+		fmt.Printf("===== Profile of %s =====", value.Name)
 		fmt.Println("\nPhone\t: ", value.Phone)
 		fmt.Println("Gender\t: ", value.Gender)
 		fmt.Println("Address\t: ", value.Address)
@@ -190,7 +189,7 @@ func UpdateProfile() {
 	user := _entities.User{}
 
 	for _, value := range resLogin {
-		fmt.Printf("==== %s's Profile Update ====", value.Name)
+		fmt.Printf("===== %s's Profile Update =====", value.Name)
 		fmt.Println("\n1. Update Name")
 		fmt.Println("2. Update Gender")
 		fmt.Println("3. Update Address")
@@ -203,34 +202,34 @@ func UpdateProfile() {
 		case "1":
 			fmt.Println("\nInput NEW Name : ")
 			fmt.Scanln(&user.Name)
-			err := _userController.UpdateName(dbConn, user.Name, value.Phone)
+			err := _controller.UpdateName(dbConn, user.Name, value.Phone)
 
 			if err != nil {
-				fmt.Println("\n==== Name Update Failed ====")
+				fmt.Println("\n===== Name Update Failed =====")
 			} else {
-				fmt.Println("\n==== Name Update Successful ====")
+				fmt.Println("\n===== Name Update Successful =====")
 			}
 
 		case "2":
 			fmt.Println("\nInput NEW Gender : ")
 			fmt.Scanln(&user.Gender)
-			err := _userController.UpdateGender(dbConn, user.Gender, value.Phone)
+			err := _controller.UpdateGender(dbConn, user.Gender, value.Phone)
 
 			if err != nil {
-				fmt.Println("\n==== Gender Update Failed ====")
+				fmt.Println("\n===== Gender Update Failed =====")
 			} else {
-				fmt.Println("\n==== Gender Update Successful ====")
+				fmt.Println("\n===== Gender Update Successful =====")
 			}
 
 		case "3":
 			fmt.Println("\nInput NEW Address : ")
 			fmt.Scanln(&user.Address)
-			err := _userController.UpdateAddress(dbConn, user.Address, value.Phone)
+			err := _controller.UpdateAddress(dbConn, user.Address, value.Phone)
 
 			if err != nil {
-				fmt.Println("\n==== Address Update Failed ====")
+				fmt.Println("\n===== Address Update Failed =====")
 			} else {
-				fmt.Println("\n==== Address Update Successful ====")
+				fmt.Println("\n===== Address Update Successful =====")
 			}
 
 		}
@@ -253,8 +252,8 @@ func UpdateProfile() {
 func DeleteProfile() {
 	user := _entities.User{}
 
-	for _, value := range resLogin {
-		fmt.Printf("==== [WARNING] Are you sure you want to delete your account? ====")
+	for range resLogin {
+		fmt.Printf("===== [WARNING] Are you sure you want to delete your account? =====")
 		fmt.Println("\n1. Yes, delete account")
 		fmt.Println("2. No, go back")
 
@@ -265,12 +264,13 @@ func DeleteProfile() {
 		case "1":
 			fmt.Println("\nInput your phone number : ")
 			fmt.Scanln(&user.Phone)
-			err := _userController.DeleteAccount(dbConn, value.Phone)
+			err := _controller.DeleteAccount(dbConn, user.Phone)
 
 			if err != nil {
-				fmt.Println("\n==== Account Delete Failed ====")
+				fmt.Println("\n===== Account Delete Failed =====")
+				fmt.Printf("\n===== Phone Number Not Found : %s =====\n", user.Phone)
 			} else {
-				fmt.Println("\n==== Account Delete Successful ====")
+				fmt.Println("\n===== Account Delete Successful =====")
 				MenuUtama()
 			}
 
@@ -287,12 +287,12 @@ func OtherProfile() {
 	fmt.Println("\nInput the phone number : ")
 	fmt.Scanln(&other.Phone)
 
-	resOther, err := _userController.GetOtherbyPhone(dbConn, other.Phone)
-	if err != nil {
-		fmt.Println("\n==== Phone Number Not Found ====")
+	resOther, err := _controller.GetOtherbyPhone(dbConn, other.Phone)
+	if err == nil {
+		fmt.Println("\n===== Phone Number Not Found =====")
 	} else {
 		for _, other := range resOther {
-			fmt.Printf("==== Profile of %s ====", other.Name)
+			fmt.Printf("===== Profile of %s =====", other.Name)
 			fmt.Println("\nPhone\t: ", other.Phone)
 			fmt.Println("Gender\t: ", other.Gender)
 			fmt.Println("Address\t: ", other.Address)
@@ -301,15 +301,13 @@ func OtherProfile() {
 }
 
 func MenuTrans() {
-	user := _entities.User{}
-
-	resBal, err := _userController.GetBalancebyPhone(dbConn, user.Phone)
+	resBal, err := _controller.GetBalancebyPhone(dbConn, phone)
 	if err != nil {
-		fmt.Println("\n==== Balance Not Found ====")
+		fmt.Println("\n===== Balance Not Found =====")
 	} else {
 		for _, user := range resBal {
 			fmt.Println("===== Transaction Menu =====")
-			fmt.Printf("===== Your Current Balance is : %d =====", user.Balance)
+			fmt.Printf("===== Your Current Balance is : %v =====", user.Balance)
 		}
 		fmt.Println("\n1. Top-Up")
 		fmt.Println("2. Transfer")
@@ -320,7 +318,7 @@ func MenuTrans() {
 
 		switch pilihan {
 		case "1":
-			// TopUp()
+			TopUp()
 		case "2":
 			// Transfer()
 		}
@@ -338,9 +336,29 @@ func MenuTrans() {
 	}
 }
 
-// func TopUp() {
+func TopUp() {
+	var topup int
+	fmt.Println("\nInsert amount to top-up : ")
+	fmt.Scanln(&topup)
 
-// }
+	err := _controller.TopUp(dbConn, topup, phone)
+	if err != nil {
+		fmt.Println("\n===== Top-Up Failed =====")
+	} else {
+		newBal, err := _controller.GetNewBalance(dbConn, phone)
+
+		if err != nil || newBal == nil {
+			fmt.Println("===== Silakan cek kembali No. HP dan Password =====")
+		}
+
+		for _, value := range newBal {
+			fmt.Println("===== Top-Up Successful =====")
+			fmt.Printf("\n===== Your New Balance is : %v =====\n", value.Balance)
+			MenuTrans()
+
+		}
+	}
+}
 
 // func Transfer() {
 
