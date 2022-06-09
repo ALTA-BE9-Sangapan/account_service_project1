a597e87133fdb62b9ce482eb3b746f6f666975a0
Artby <artbydhimathera@gmail.com>
2022-06-09 13:45:37 +0700

    Added Update Profile Feature

--- main.go
+++ main.go
@@ -1,32 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"project1/config"
-	"project1/controllers"
-	"project1/entities"
-
-	"gorm.io/gorm"
+	"os"
+	_config "project1/config"
+	_userController "project1/controllers"
+	_entities "project1/entities"
 )
 
-var dbConn *gorm.DB
+//Global Variables
+var dbConn *sql.DB
 var cond bool = true
 var pilihan string = ""
+var resLogin []_entities.User
+var err error
+var phone string
+var pass string
 
 func init() {
-	dbConn = config.ConnectDB()
-	InitialMigration()
-}
-
-func InitialMigration() {
-	dbConn.AutoMigrate(&entities.User{})
-
+	dbConn = _config.ConnectDB()
 }
 
 func MenuUtama() {
 
 	// Menu Utama
-	// myMenu := []string{"1. Login", "2. Register", "3. Lihat Profile", "4. Edit Profile", "5. Delete Akun", "6. Top Up", "7. Transfer", "8. History Top Up", "9. History Transfer", "10. Friends Profile Check"}
 
 	fmt.Println("===== Menu Utama =====")
 	fmt.Println("1. Login")
@@ -54,7 +52,7 @@ func MenuUtama() {
 // Menu Login
 func Login() {
 
-	user := entities.User{}
+	user := _entities.User{}
 
 	fmt.Println("===== Login =====")
 	fmt.Println("Masukkan No. HP :")
@@ -62,11 +60,23 @@ func Login() {
 	fmt.Println("Masukkan Password :")
 	fmt.Scanln(&user.Password)
 
-	resLogin := controllers.GetUserbyPassword(dbConn, user.Password)
+	phone = user.Phone
+	pass = user.Password
+
+	UserMenu(phone, pass)
+
+}
+
+//Menu User
+func UserMenu(phone string, pass string) {
+	resLogin, err = _userController.GetUserbyPhone(dbConn, phone, pass)
+
+	if err != nil || resLogin == nil {
+		fmt.Println("==== Silakan cek kembali No. HP dan Password ====")
+	}
 
 	for _, value := range resLogin {
 		for pilihan != "0" {
-			fmt.Println("===== Login Berhasil =====")
 			fmt.Println("===== Selamat Datang,", value.Name, "=====")
 			fmt.Println("===== Menu User =====")
 			fmt.Println("1. Profil User")
@@ -79,7 +89,7 @@ func Login() {
 
 			switch pilihan {
 			case "1":
-				// ProfilUser()
+				UserProfile()
 			case "2":
 				// MenuTrans()
 			case "3":
@@ -94,60 +104,146 @@ func Login() {
 				fmt.Scanln(&pilihan)
 
 				if pilihan == "2" {
-					resLogin = nil
 					MenuUtama()
 				}
 			}
 		}
-	}
-
-	if resLogin != nil {
-		fmt.Println("==== Akun tidak ditemukan ====")
 	}
 }
 
 // Menu Register
 func Register() {
 
-	user := entities.User{}
+	newUser := _entities.User{}
 
 	fmt.Println("===== Registrasi =====")
 	fmt.Println("Masukkan Nama :")
-	fmt.Scanln(&user.Name)
+	fmt.Scanln(&newUser.Name)
 	fmt.Println("Masukkan No. HP :")
-	fmt.Scanln(&user.Phone)
+	fmt.Scanln(&newUser.Phone)
 	fmt.Println("Masukkan Password :")
-	fmt.Scanln(&user.Password)
+	fmt.Scanln(&newUser.Password)
 	fmt.Println("Masukkan Gender :")
-	fmt.Scanln(&user.Gender)
+	fmt.Scanln(&newUser.Gender)
 	fmt.Println("Masukkan Address :")
-	fmt.Scanln(&user.Address)
+	fmt.Scanln(&newUser.Address)
 
-	resRegis := controllers.CreateUser(dbConn, user)
+	err := _userController.CreateUser(dbConn, newUser)
 
-	if resRegis != nil {
-		fmt.Println("Nomor HP sudah terdaftar")
+	if err != nil {
+		fmt.Println("==== Nomor HP sudah terdaftar ====")
 	} else {
-		fmt.Println("Registrasi berhasil")
+		fmt.Println("==== Registrasi berhasil ====")
 	}
 }
 
 func UserProfile() {
+	resLogin, err = _userController.GetUserbyPhone(dbConn, phone, pass)
+	for pilihan != "0" {
+		fmt.Println("===== Profile Menu =====")
+		fmt.Println("1. Read Profile")
+		fmt.Println("2. Update Profile")
+		fmt.Println("3. Delete Profile")
+		fmt.Println("4. Other Users Profile")
+		fmt.Println("0. Kembali ke Menu User")
+		fmt.Print("Masukkan Pilihan : ")
 
-	fmt.Println("===== Profile =====")
+		fmt.Scanln(&pilihan)
+
+		switch pilihan {
+		case "1":
+			ReadProfile()
+		case "2":
+			UpdateProfile()
+		case "3":
+			// RiwayatTrans()
+		}
+
+		if pilihan == "0" {
+			fmt.Println("===== Kembali ke menu user? =====")
+			fmt.Println("1. Tetap di halaman")
+			fmt.Println("2. Kembali ke menu user")
+			fmt.Print("Masukkan Pilihan : ")
+			fmt.Scanln(&pilihan)
+
+			if pilihan == "2" {
+				UserMenu(phone, pass)
+			}
+		}
+	}
 
 }
+
+func ReadProfile() {
+	resLogin, err = _userController.GetUserbyPhone(dbConn, phone, pass)
+	for _, value := range resLogin {
+		fmt.Printf("==== Profile of %s ====", value.Name)
+		fmt.Println("\nPhone\t: ", value.Phone)
+		fmt.Println("Gender\t: ", value.Gender)
+		fmt.Println("Address\t: ", value.Address)
+	}
+}
+
 func UpdateProfile() {
+	user := _entities.User{}
 
-	fmt.Println("===== Update Profile =====")
-	fmt.Println("1. Ganti Nama\n2. Ubah Tanggal Lahir\n3. Ubah Jenis Kelamin\n4. Ubah Alamat\n99. Kembali")
+	for _, value := range resLogin {
+		fmt.Printf("==== %s's Profile Update ====", value.Name)
+		fmt.Println("\n1. Update Name")
+		fmt.Println("2. Update Gender")
+		fmt.Println("3. Update Address")
+		fmt.Println("0. Kembali ke Menu Profile")
 
-	var input int
-	fmt.Println("Masukkan Pilihan : ")
-	fmt.Scanf("%d", &input)
-	switch input {
-	case 1:
+		fmt.Print("Masukkan Pilihan : ")
+		fmt.Scanln(&pilihan)
 
+		switch pilihan {
+		case "1":
+			fmt.Println("\nInput NEW Name : ")
+			fmt.Scanln(&user.Name)
+			err := _userController.UpdateName(dbConn, user.Name, value.Phone)
+
+			if err != nil {
+				fmt.Println("\n==== Name Update Failed ====")
+			} else {
+				fmt.Println("\n==== Name Update Successful ====")
+			}
+
+		case "2":
+			fmt.Println("\nInput NEW Gender : ")
+			fmt.Scanln(&user.Gender)
+			err := _userController.UpdateGender(dbConn, user.Gender, value.Phone)
+
+			if err != nil {
+				fmt.Println("\n==== Gender Update Failed ====")
+			} else {
+				fmt.Println("\n==== Gender Update Successful ====")
+			}
+
+		case "3":
+			fmt.Println("\nInput NEW Address : ")
+			fmt.Scanln(&user.Address)
+			err := _userController.UpdateAddress(dbConn, user.Address, value.Phone)
+
+			if err != nil {
+				fmt.Println("\n==== Address Update Failed ====")
+			} else {
+				fmt.Println("\n==== Address Update Successful ====")
+			}
+
+		}
+
+		if pilihan == "0" {
+			fmt.Println("===== Kembali ke menu profile? =====")
+			fmt.Println("1. Tetap di halaman")
+			fmt.Println("2. Kembali ke menu profile")
+			fmt.Print("Masukkan Pilihan : ")
+			fmt.Scanln(&pilihan)
+
+			if pilihan == "2" {
+				UserProfile()
+			}
+		}
 	}
 
 }
@@ -162,6 +258,7 @@ func Keluar() {
 	if pilihan == "2" {
 		cond = false
 		fmt.Println("===== Terima Kasih =====")
+		os.Exit(0)
 	}
 }
 
